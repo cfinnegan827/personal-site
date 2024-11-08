b366c34fda15e90d699cffa693a2fcac1f581dd4
Connor Finnegan <cfinnegan827@gmail.com>
2024-11-07 19:25:17 -0500

    add navbar and page routes

---
+++ src/pages/Education.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Education (){
+  return (
+    <div>
+      <h2>Contact Page</h2>
+      <p>Welcome to the Education page!</p>
+    </div>
+  );
+}
+
+export default Education;
