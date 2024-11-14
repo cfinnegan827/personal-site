4bf43bd3310a73b01bebda3f442c2ba3653d1453
Connor Finnegan <cfinnegan827@gmail.com>
2024-11-14 15:11:19 -0500

    add scroll snap with video animated background

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React, { useRef } from 'react';
 import '../styles/Home.scss';
+import '../styles/typing.scss'
+import '../styles/Background_mp4.scss'
 
 const Home = () => {
     const scrollContainer = useRef(null);
-  
+    const textRef = React.useRef(null);
     const handleScroll = () => {
         const container = scrollContainer.current;
         const sectionHeight = container.clientHeight;
@@ -17,8 +19,16 @@ const Home = () => {
             ref={scrollContainer}
             onScroll={handleScroll}
             className="scroll-container">
-            <div className="section">
-                
+            <div className="section video-background">
+                <div className="video-background-container">
+                    <video autoPlay muted loop>
+                        <source src="/images/winter-bus-pixel.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                    </video>
+                </div>
+                <div ref={textRef} className="typing">
+                    <h1>Welcome to My Portfolio Site!</h1>
+                </div>
             </div>
             <div className="section">Section 2</div>
             <div className="section">Section 3</div>
